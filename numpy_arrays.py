d319bdb682998866aaef2d105e0de9434e8ad5c8
Paul Hicks <126404389+paulhicks01@users.noreply.github.com>
2023-08-31 12:58:44 +0100

    Add files via upload

---
+++ numpy_arrays.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+np.random.random((2,4))
+
+
+
+#import numpy as np
+#from matplotlib import pyplot as plt
+#plt.scatter(np.arange(0, 7),
+#            np.arange(-3,4))
+#plt.show()
+
+
+
+#import numpy as np
+#from matplotlib import pyplot as plt
+
+#rgb = np.array([[[255, 0, 0],[255, 0, 0],[255, 0, 0]],
+#                [[0, 255, 0],[0, 255, 0],[0, 255, 0]],
+#                [[0, 0, 255],[0, 0, 255],[0, 0, 255]]])
+#plt.imshow(rgb)
+#plt.show()
+
+
+
+#import numpy as np
+#from matplotlib import pyplot as plt
+
+#rgb = np.array([[[255, 0, 0],[255, 255, 0],[255, 255, 255]],
+#                [[255, 0, 255],[0, 255, 0],[0, 255, 255]],
+#                [[0, 0, 0],[0, 255, 255],[0, 0, 255]]])
+#plt.imshow(rgb)
+#plt.show()
+
+
+# In[ ]:
+
+
+
+
